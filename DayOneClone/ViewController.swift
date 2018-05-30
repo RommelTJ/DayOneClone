6b219fe0b04f50b8ebaf2264b3e8f3e0f498b586
Rommel Rico <me@rommelrico.com>
2018-05-30 08:50:29 -0700

    Initial commit

---
+++ DayOneClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DayOneClone
+//
+//  Created by Rommel Rico on 5/30/18.
+//  Copyright © 2018 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
