1cf40a6f73c028b0c505d1e2c748d8e229c08b15
Rommel Rico <me@rommelrico.com>
2018-05-31 20:57:51 -0700

    Setting up IBOutlets

--- DayOneClone/CreateJournalViewController.swift
+++ DayOneClone/CreateJournalViewController.swift
@@ -10,26 +10,30 @@ import UIKit
 
 class CreateJournalViewController: UIViewController {
 
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var journalTextView: UITextView!
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var setDateButton: UIButton!
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelTapped(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+    @IBAction func setDateTapped(_ sender: Any) {
+    }
+    
+    @IBAction func blueCalendarTapped(_ sender: Any) {
+    }
+    
+    @IBAction func blueCameraTapped(_ sender: Any) {
+    }
+    
 }
