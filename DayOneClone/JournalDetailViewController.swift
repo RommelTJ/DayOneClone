47532c7535ae4ccf0231715484cf6477345eeee8
Rommel Rico <me@rommelrico.com>
2018-06-02 15:16:02 -0700

    Setting up the Entry in the Journal Detail View Controller

--- DayOneClone/JournalDetailViewController.swift
+++ DayOneClone/JournalDetailViewController.swift
@@ -12,9 +12,26 @@ class JournalDetailViewController: UIViewController {
 
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var journalTextLabel: UILabel!
+    private var entry: Entry?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let entry = self.entry {
+            title = entry.datePrettyString()
+            journalTextLabel.text = entry.text
+            
+            for picture in entry.pictures {
+                let imageView = UIImageView()
+                imageView.contentMode = .scaleAspectFit
+                let ratio = picture.fullImage().size.height / picture.fullImage().size.width
+                imageView.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 1.0)
+                imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: ratio).isActive = true
+                imageView.image = picture.fullImage()
+                stackView.addArrangedSubview(imageView)
+            }
+        } else {
+            journalTextLabel.text = ""
+        }
     }
 
 }
