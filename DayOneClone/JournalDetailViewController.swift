e0fa402c82e68a99821d4fde2f75cc6535bde1fe
Rommel Rico <me@rommelrico.com>
2018-06-02 15:10:24 -0700

    Implementing the Detail View stack view and text label

--- DayOneClone/JournalDetailViewController.swift
+++ DayOneClone/JournalDetailViewController.swift
@@ -10,26 +10,11 @@ import UIKit
 
 class JournalDetailViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var journalTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
